318344d9107cbb9b05a7ef3865bcc4196b4e9fbc
Dmitriy Kalachev <dima.kalachov@gmail.com>
2017-01-30 12:52:19 +0300

    Carthage support

---
+++ Example/CenterFlowLayout/CenterFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  CenterFlowLayout.h
+//  CenterFlowLayout
+//
+//  Created by Dmitriy Kalachev on 30/01/2017.
+//  Copyright © 2017 keighl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CenterFlowLayout.
+FOUNDATION_EXPORT double CenterFlowLayoutVersionNumber;
+
+//! Project version string for CenterFlowLayout.
+FOUNDATION_EXPORT const unsigned char CenterFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CenterFlowLayout/PublicHeader.h>
+#import <CenterFlowLayout/KTCenterFlowLayout.h>
+
